c0bf8db1f7df9488b43a5e8360e40bcf1151596f
Eric Liu <eric.liu@uber.com>
2018-06-13 12:49:35 -0700

    Linked outlets

---
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Eric Liu on 6/13/18.
+//  Copyright © 2018 Eric Liu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var serialNumberField: UITextField!
+    @IBOutlet weak var valueField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    
+}
