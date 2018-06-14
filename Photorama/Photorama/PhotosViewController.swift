0263cf111c69543f2f932cc98a4f1f5ed4fe20ce
Eric Liu <eric.liu@uber.com>
2018-06-14 11:26:00 -0700

    Creates Photorama.

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Eric Liu on 6/14/18.
+//  Copyright © 2018 Eric Liu. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
