c0819a35c3920bc43e246eeca94a5512c9d361c8
Eric Liu <eric.liu@uber.com>
2018-06-14 15:25:10 -0700

    Update image.

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -25,7 +25,7 @@ enum PhotoError: Error {
 }
 
 class PhotoStore {
-    
+
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -36,28 +36,60 @@ class PhotoStore {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
-            
+
             if let jsonData = data {
-                
-            let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
-            
-                
+
+                let result = self.processPhotosRequest(data: data, error: error)
+                completion(result)
+
+
             } else if let requestError = error {
                 print("Error fetching interesting photos: \(requestError)")
             } else {
                 print("Unexpected error with the request")
             }
         }
-        
+
         task.resume()
     }
-    
+
     private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
         guard let jsonData = data else {
             return .failure(error!)
         }
-        
+
         return FlickrAPI.photos(fromJson: jsonData)
+    }
+
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+
+        let task = session.dataTask(with: request) {
+            (data, response, error ) -> Void in
+
+            let result = self.processImageRequest(data: data, error: error)
+            completion(result)
+        }
+
+        task.resume()
+    }
+
+
+    func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+                let imageData = data,
+                let image = UIImage(data: imageData)
+                else {
+
+            // Couldn't create an image
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+
+        return .success(image)
     }
 }
--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
-    
+
     @IBOutlet var imageView: UIImageView!
     var photoStore: PhotoStore!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         photoStore.fetchInterestingPhotos { (photosResult) -> Void in
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
+
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
+
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
             }
@@ -32,6 +37,22 @@ class PhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func updateImageView(for photo: Photo) {
+        photoStore.fetchImage(for: photo) {
+            (imageResult) -> Void in
+
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+
+            case let .failure(error):
+                print("Error downloading image \(error)")
+            }
+
+
+        }
+    }
 
 }
 
