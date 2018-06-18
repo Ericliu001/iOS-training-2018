3f93e85b3ac467fce479f310b91e6f4f46c3db22
Eric Liu <eric.liu@uber.com>
2018-06-17 22:42:05 -0700

    New training examples.

---
+++ Cecil_Costa_Training/Cecil Costa Training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cecil Costa Training
+//
+//  Created by Eric Liu on 6/17/18.
+//  Copyright © 2018 Eric Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
